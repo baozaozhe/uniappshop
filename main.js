5da349673de0cd88c42affc05ffe56e28cc7fa45
muxulong <1658562850@qq.com>
2021-10-14 15:14:17 +0800

    HOME开发

--- main.js
+++ main.js
@@ -3,6 +3,36 @@
 import Vue from 'vue'
 import App from './App'
 
+
+import { $http } from '@escook/request-miniprogram'
+
+uni.$http = $http
+// 配置请求根路径
+$http.baseUrl = 'https://www.uinav.com'
+
+// 请求开始之前做一些事情
+$http.beforeRequest = function (options) {
+  uni.showLoading({
+    title: '数据加载中...',
+  })
+}
+
+// 封装的展示消息提示的方法
+uni.$showMsg = function (title = '数据加载失败！', duration = 1500) {
+  uni.showToast({
+    title,
+    duration,
+    icon: 'none',
+  })
+}
+// 请求完成之后做一些事情
+$http.afterRequest = function () {
+  uni.hideLoading()
+}
+
+
+
+
 Vue.config.productionTip = false
 
 App.mpType = 'app'
